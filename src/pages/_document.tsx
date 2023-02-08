d7d8462b66cf8cb55c09306b30b34a46538b9131
Jeongwoo <jeongwoo@ifgroup.io>
2023-02-08 19:39:45 +0900

    ENH: add metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang='en'>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <meta name='description' content='Multiple Replace Online' />
+                <meta name='author' content='awmaker' />
+                <meta
+                    name='keywords'
+                    content='multiple, replace, online, tool, editor, text, file, string, regex'
+                />
+                <meta name='theme-color' content='#000000' />
+                <meta name='msapplication-TileColor' content='#000000' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
@@ -53,83 +54,89 @@ export default function Home() {
     }
 
     return (
-        <main className={styles.main}>
-            <h1>Multiple Replace Online</h1>
-            <br />
-            <form onSubmit={(e) => e.preventDefault()}>
-                <h3>Separator</h3>
-                <div className={styles.divide}>
-                    <label>Separator: </label>
-                    <input
-                        name='split'
-                        type='text'
-                        value={split}
-                        onChange={handleSplitChange}
-                    />
-                </div>
-                <h3>Rule</h3>
-                <div className={styles.divide}>
-                    <label htmlFor='file'>Rule File: </label>
-                    <input
-                        name='rule'
-                        type='file'
-                        accept='.txt'
-                        onChange={handleRuleChange}
-                    />
-                    <div>
-                        Rule Example
-                        <p>A{split}A`</p>
-                        <p>B{split}B`</p>
+        <>
+            <Head>
+                <title>Multiple Replace Online</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1>Multiple Replace Online</h1>
+                <br />
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <h3>Separator</h3>
+                    <div className={styles.divide}>
+                        <label>Separator: </label>
+                        <input
+                            name='split'
+                            type='text'
+                            value={split}
+                            onChange={handleSplitChange}
+                        />
                     </div>
-                </div>
-                <h3>Target File</h3>
-                <div className={styles.divide}>
-                    <label htmlFor='file'>Target File: </label>
-                    <input
-                        name='file'
-                        type='file'
-                        accept='.txt'
-                        onChange={handleFileChange}
-                    />
-                </div>
-                {result && (
-                    <>
-                        <h3>Download Result</h3>
-                        <div className={styles.divide}>
-                            <input
-                                type='button'
-                                value='Download'
-                                onClick={downloadTxtFile}
-                            />
+                    <h3>Rule</h3>
+                    <div className={styles.divide}>
+                        <label htmlFor='file'>Rule File: </label>
+                        <input
+                            name='rule'
+                            type='file'
+                            accept='.txt'
+                            onChange={handleRuleChange}
+                        />
+                        <div>
+                            Rule Example
+                            <p>A{split}A`</p>
+                            <p>B{split}B`</p>
                         </div>
-                    </>
-                )}
-                {(rule || file) && <hr />}
-                {rule && (
-                    <>
-                        <h3>rule</h3>
-                        <div className={styles.divide}>
-                            <pre>{rule}</pre>
-                        </div>
-                    </>
-                )}
-                {file && (
-                    <>
-                        <h3>file</h3>
-                        <div className={styles.divide}>
-                            <pre>{file}</pre>
-                        </div>
-                    </>
-                )}
-                {result && (
-                    <>
-                        <h3>result</h3>
-                        <div className={styles.divide}>
-                            <pre>{result}</pre>
-                        </div>
-                    </>
-                )}
-            </form>
-        </main>
+                    </div>
+                    <h3>Target File</h3>
+                    <div className={styles.divide}>
+                        <label htmlFor='file'>Target File: </label>
+                        <input
+                            name='file'
+                            type='file'
+                            accept='.txt'
+                            onChange={handleFileChange}
+                        />
+                    </div>
+                    {result && (
+                        <>
+                            <h3>Download Result</h3>
+                            <div className={styles.divide}>
+                                <input
+                                    type='button'
+                                    value='Download'
+                                    onClick={downloadTxtFile}
+                                />
+                            </div>
+                        </>
+                    )}
+                    {(rule || file) && <hr />}
+                    {rule && (
+                        <>
+                            <h3>rule</h3>
+                            <div className={styles.divide}>
+                                <pre>{rule}</pre>
+                            </div>
+                        </>
+                    )}
+                    {file && (
+                        <>
+                            <h3>file</h3>
+                            <div className={styles.divide}>
+                                <pre>{file}</pre>
+                            </div>
+                        </>
+                    )}
+                    {result && (
+                        <>
+                            <h3>result</h3>
+                            <div className={styles.divide}>
+                                <pre>{result}</pre>
+                            </div>
+                        </>
+                    )}
+                </form>
+            </main>
+        </>
     )
 }
